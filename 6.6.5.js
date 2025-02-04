b4906116ff3c699d76075685bb60bc90c088fc67
VitalySLTfr <89817215938@yandex.ru>
2025-02-04 21:29:30 +0300

    Create 6.6.5.js

---
+++ 6.6.5.js
@@ -0,0 +1,18 @@
+const obj = {
+    some: 'some',
+    dom: 'text',
+    arr: [1, 2, 3, 4, 5],
+    tom: 'there'
+};
+const arrValues = [];
+for (let i in obj) {
+    if (Array.isArray(obj[i])) {
+        for (let j = 0; j < obj[i].length; j+=1) {
+            arrValues.push(obj[i][j]);
+        }
+    }
+    else {  
+            arrValues.push(obj[i]);
+        }
+}
+   console.log(arrValues);
